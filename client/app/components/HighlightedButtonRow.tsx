4d28736b02665ff70d60de544fafb21f9257b99d
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2023-12-13 23:42:13 +0500

    updates

--- client/app/components/HighlightedButtonRow.tsx
+++ client/app/components/HighlightedButtonRow.tsx
@@ -12,7 +12,7 @@ const HighlightedButtonRow = (props: HighlightedButtonRowProp) => {
   return (
     <div>
       <div className={`${styles.row} ${style ? styles.row_style : ""}`}>
-        <span className={`${styles.title} ${style ? styles.title_style : ""}`}>{title}</span>
+        <span className={`font-size-14 ${styles.title} ${style ? styles.title_style : ""}`}>{title}</span>
         <button
           type="button"
           className="btn btn-primary"
