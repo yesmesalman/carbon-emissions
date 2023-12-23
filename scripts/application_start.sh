3c4eab59a0b6e31d689adb2073cf0acf772dce5d
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2023-12-23 18:10:02 +0500

    CI/CD file updates

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/carbon-emissions/deploy.log
 
-echo 'pm2 restart next-app' >> /home/ec2-user/carbon-emissions/deploy.log
-pm2 restart next-app >> /home/ec2-user/carbon-emissions/deploy.log
+echo 'pm2 restart carbonApp' >> /home/ec2-user/carbon-emissions/deploy.log
+pm2 restart carbonApp >> /home/ec2-user/carbon-emissions/deploy.log
