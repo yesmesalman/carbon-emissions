064f8fc25b903852608998b2083e5453838b32e3
Salman Saleem <37062860+yesmesalman@users.noreply.github.com>
2023-12-27 22:55:45 +0500

    updated pipeline

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,6 +6,3 @@ cd /home/ec2-user/carbon-emissions/client >> /home/ec2-user/carbon-emissions/dep
 
 echo 'npm install' >> /home/ec2-user/carbon-emissions/deploy.log 
 npm install >> /home/ec2-user/carbon-emissions/deploy.log
-
-echo 'npm run build' >> /home/ec2-user/carbon-emissions/deploy.log 
-npm run build >> /home/ec2-user/carbon-emissions/deploy.log
